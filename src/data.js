d8a781caecbcc433293d0ea467dfd09dc2c0d91a
Mathias Bynens <mathias@qiwi.be>
2013-06-05 13:18:54 +0200

    Add `wrap` option
    Closes #2.

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 var regenerate = require('regenerate');
+var fs = require('fs');
 
 var set = regenerate()
 	.addRange(0x20, 0x7E) // printable ASCII symbols
@@ -7,5 +8,6 @@ var set = regenerate()
 	.remove('\\');        // not `\`
 
 module.exports = {
-	'whitelist': set.toString()
+	'whitelist': set.toString(),
+	'version': JSON.parse(fs.readFileSync('package.json', 'utf8')).version
 };
