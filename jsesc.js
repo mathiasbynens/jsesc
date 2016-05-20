24455b45733040b864ea19844e94e013b1e32ceb
Mathias Bynens <mathias@qiwi.be>
2016-05-20 15:16:32 +0200

    Release v1.1.0

--- jsesc.js
+++ jsesc.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/jsesc v1.0.0 by @mathias */
+/*! https://mths.be/jsesc v1.1.0 by @mathias */
 ;(function(root) {
 
 	// Detect free variables `exports`
@@ -251,7 +251,7 @@
 		return result;
 	};
 
-	jsesc.version = '1.0.0';
+	jsesc.version = '1.1.0';
 
 	/*--------------------------------------------------------------------------*/
 
