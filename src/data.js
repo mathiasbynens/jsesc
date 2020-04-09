cca83b4092aac596ff831a583cead72c560cd269
Justin Ridgewell <justin@ridgewell.name>
2020-04-09 15:18:25 -0400

    Escape non-ASCII whitespace in minimal mode (#62)
    Co-authored-by: Mathias Bynens <mathias@qiwi.be>

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const regenerate = require('regenerate');
+const whitespaceCodePoints = require('unicode-13.0.0/General_Category/Space_Separator/code-points.js');
 
 const set = regenerate()
 	.addRange(0x20, 0x7E) // printable ASCII symbols
@@ -9,7 +10,12 @@ const set = regenerate()
 	.remove('\\')         // not `\`
 	.remove('`');         // not '`'
 
+const whitespace = regenerate(whitespaceCodePoints)
+	.removeRange(0x00, 0x7F)
+	.add(0x2028, 0x2029);
+
 module.exports = {
 	'whitelist': set.toString().slice(1, -1),
+	'whitespace': whitespace.toString(),
 	'version': require('../package.json').version
 };
